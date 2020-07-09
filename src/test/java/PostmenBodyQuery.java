407e77df35bfba77e4bf6e7c982cb11566af00e0
Alexandr Polyakov <PolyakovQA@yandex.ru>
2020-07-09 13:35:46 +0300

    corrected yml

---
+++ src/test/java/PostmenBodyQuery.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmenBodyQuery {
+
+    @Test
+    public void ShouldTestBody() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Body")
+                .contentType("text/plain; charset=UTF-8")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo ("Body"))
+        ;
+    }
+}
+
